42faa816ca710646766bed6f6c97ee94633bd5a8
vinzi13 <kelvinhalim45@gmail.com>
2017-12-26 15:33:14 +0700

    fix pulsa providers empty response

--- src/pulsa.php
+++ src/pulsa.php
@@ -3,7 +3,7 @@ namespace satuh;
 use InvalidArgumentException;
 class pulsa
 {
-    const URL = "https://epay.satuh.com/api/pulsa/";
+    const URL = "https://epay.satuh.com/api/pulsa";
     protected $accessToken = null;
     protected $clientId;
     protected $clientSecret;
@@ -60,28 +60,28 @@ class pulsa
 
     function products($provider_id){
         if (empty($provider_id)) throw new InvalidArgumentException("Provider ID is not specified");
-        return $this->httpBuilder->get(self::URL.'product/'.$provider_id);
+        return $this->httpBuilder->get(self::URL.'/product/'.$provider_id);
     }
 
     function productDetail($product_id){
         if (empty($product_id)) throw new InvalidArgumentException("Product ID is not specified");
-        return $this->httpBuilder->get(self::URL.'product-detail/'.$product_id);
+        return $this->httpBuilder->get(self::URL.'/product-detail/'.$product_id);
     }
 
     function transaction($product_id,$phone){
         if (empty($product_id)) throw new InvalidArgumentException("Product ID is not specified");
         if (empty($phone)) throw new InvalidArgumentException("Phone is not specified");
-        return $this->httpBuilder->post(self::URL.'transaction',['phone'=>$phone,'product_id'=> $product_id]);
+        return $this->httpBuilder->post(self::URL.'/transaction',['phone'=>$phone,'product_id'=> $product_id]);
 
     }
 
     function transactionDetail($pulsa_transaction_id){
         if (empty($pulsa_transaction_id)) throw new InvalidArgumentException("Pulsa Transaction ID is not specified");
-        return $this->httpBuilder->get(self::URL.'transaction-detail/'.$pulsa_transaction_id);
+        return $this->httpBuilder->get(self::URL.'/transaction-detail/'.$pulsa_transaction_id);
     }
 
     function transactionHistory(){
-        return $this->httpBuilder->get(self::URL.'transaction-history/');
+        return $this->httpBuilder->get(self::URL.'/transaction-history/');
     }
 
 }
